b43e88eafac4cad95f6cfe305c8689c6541c636b
shiielty <56679623+Shiielty@users.noreply.github.com>
2023-06-19 00:51:29 +0700

    Create Navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,14 @@
+import "./Navbar.css";
+
+export default function Navbar() {
+  return (
+    <nav>
+      <h1>Find Us!</h1>
+      <div className="target-list">
+        <div>Mario</div>
+        <div>Harvest Moon</div>
+        <div>Star</div>
+      </div>
+    </nav>
+  );
+}

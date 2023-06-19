39e7a81dcc9c5481dd8a40f472afae4078a71c70
shiielty <56679623+Shiielty@users.noreply.github.com>
2023-06-19 18:53:55 +0700

    setup firebase

---
+++ src/firebase-config.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDNbN9hoZvSj7DlqSGnlyK8bbHwgetMeC0",
+  authDomain: "photo-tag-game-56181.firebaseapp.com",
+  projectId: "photo-tag-game-56181",
+  storageBucket: "photo-tag-game-56181.appspot.com",
+  messagingSenderId: "473408391754",
+  appId: "1:473408391754:web:b28a88034a2eb0a358f4b3",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
